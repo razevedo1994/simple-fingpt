e6f07ade47a8b831bda9d9c10bcd75721c6a3a34
razevedo1994 <razevedo.contato@gmail.com>
2026-03-06 07:59:21 -0300

    Add try/catch on collection creation

--- project/ingestion.py
+++ project/ingestion.py
@@ -1,15 +1,25 @@
 from qdrant_client import QdrantClient, models
 from fastembed import TextEmbedding
-from config.settings import COLLECTION_NAME
+from config.settings import COLLECTION_NAME, FILE_PATH
 
 qdrant = QdrantClient(
     url="http://localhost:6333",
 )
 
-qdrant.create_collection(
-    collection_name=COLLECTION_NAME,
-    vectors_config=models.VectorParams(
-        size=384,
-        distance=models.Distance.COSINE,
-    ),
-)
+try:
+    if not qdrant.collection_exists(collection_name=COLLECTION_NAME):
+
+        qdrant.create_collection(
+            collection_name=COLLECTION_NAME,
+            vectors_config=models.VectorParams(
+                size=384,
+                distance=models.Distance.COSINE,
+            ),
+        )
+except Exception as e:
+    print(e)
+
+
+with open(FILE_PATH, "r", encoding="utf-8") as file:
+    content = file.read()
+    print(content)
