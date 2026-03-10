cfec941f1d09c3fc15ec2a78589319cdb9cbccac
razevedo1994 <razevedo.contato@gmail.com>
2026-03-10 07:02:58 -0300

    Update ingestion importing create_collection method

--- project/ingestion.py
+++ project/ingestion.py
@@ -1,24 +1,12 @@
-from qdrant_client import QdrantClient, models
 from fastembed import TextEmbedding
-from config.settings import COLLECTION_NAME, FILE_PATH
+from storage.vector_storage import create_collection
+from config.settings import FILE_PATH, COLLECTION_NAME, QDRANT_ENDPOINT
 
-qdrant = QdrantClient(
-    url="http://localhost:6333",
+
+create_collection(
+    collection=COLLECTION_NAME,
+    endpoint=QDRANT_ENDPOINT,
 )
-
-try:
-    if not qdrant.collection_exists(collection_name=COLLECTION_NAME):
-
-        qdrant.create_collection(
-            collection_name=COLLECTION_NAME,
-            vectors_config=models.VectorParams(
-                size=384,
-                distance=models.Distance.COSINE,
-            ),
-        )
-except Exception as e:
-    print(e)
-
 
 with open(FILE_PATH, "r", encoding="utf-8") as file:
     content = file.read()
