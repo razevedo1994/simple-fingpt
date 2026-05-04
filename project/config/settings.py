b1e1ebf02eadaceefae17f18143746fbe5cb59ce
Rodrigo Azevedo <razevedo.contato@gmail.com>
2026-05-04 18:06:54 -0300

    Update constants

--- project/config/settings.py
+++ project/config/settings.py
@@ -4,3 +4,5 @@ FILE_PATH = "./config/AAPL_10-K_1A_temp.md"
 QDRANT_ENDPOINT = "http://localhost:6333"
 SPARSE_MODEL = "Qdrant/bm25"
 COLBERT_MODEL = "colbert-ir/colbertv2.0"
+MAX_TOKENS = 300
+EMAIL = ""
